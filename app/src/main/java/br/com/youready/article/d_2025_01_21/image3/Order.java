d80aa69946a80d3238df213d530ff7bfb4b10a38
Rafael Issao Miyagawa <rafael.issao@gmail.com>
2025-09-25 17:08:17 -0300

    Comment on Order

--- app/src/main/java/br/com/youready/article/d_2025_01_21/image3/Order.java
+++ app/src/main/java/br/com/youready/article/d_2025_01_21/image3/Order.java
@@ -2,6 +2,7 @@ package br.com.youready.article.d_2025_01_21.image3;
 
 import java.util.List;
 
+// O método extraído vem para o record Order
 record Order(Long id, List<Item> items) {
     public double getTotal() {
         double totalOrder = 0;
