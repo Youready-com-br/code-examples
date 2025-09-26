8f77040b1c1c9788a63902d991d2c0418aa18978
Rafael Issao Miyagawa <rafael.issao@gmail.com>
2025-09-26 16:02:42 -0300

    Another fix!

--- app/src/main/groovy/br/com/youready/article/d_2025_03_10/image1/SleepSort.groovy
+++ app/src/main/groovy/br/com/youready/article/d_2025_03_10/image1/SleepSort.groovy
@@ -1,16 +1,14 @@
-package br.com.youready.article.d_2025_03_10.image1
+package br.com.youready.article.d_2025_03_10.image1;
 
-import java.util.concurrent.Executors
+import java.util.concurrent.Executors;
 
-class SleepSort {
-    static void sort(List<Integer> unsorted) {
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            unsorted.each { number ->
-                executor.submit() -> {
-                    Thread.sleep(number * 100)
-                    println(number)
-                }
-            }
+var unsorted = [3, 5, 2, 1, 4]
+
+try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+    unsorted.each { number ->
+        executor.submit() {
+            Thread.sleep(number * 100)
+            println(number)
         }
     }
 }
